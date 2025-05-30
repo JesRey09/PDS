e909028375a2f53c6e26bdb58e8bb8ffee3555bd
JesRey09 <jesusjarf2004@gmail.com>
2025-05-29 23:04:10 -0600

    add_file

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import sys
-from src.actividad_1 import continuous_sine, discrete_sine
-from src.actividad_2 import understanding_freq
+from src.Tarea1 import (
+    continuous_sine, discrete_sine,
+    continuous_exponential, discrete_exponential,
+    continuous_triangle, discrete_triangle,
+    continuous_square, discrete_square
+)
+from src.Tarea2 import understanding_freq
 
 
 def main(options):
-    if options[1] == "act_1":
-        continuous_sine()
-        discrete_sine()
-    elif options[1] == "act_2":
-        if len(args) > 2:
+    if options[1] == "Tarea1":
+        if len(options) < 3:
+            print("Por favor especifica el tipo de señal: seno, exponencial, triangular, cuadrada")
+            return
+
+        signal_type = options[2].lower()
+
+        if signal_type == "seno":
+            continuous_sine()
+            discrete_sine()
+        elif signal_type == "exponencial":
+            continuous_exponential()
+            discrete_exponential()
+        elif signal_type == "triangular":
+            continuous_triangle()
+            discrete_triangle()
+        elif signal_type == "cuadrada":
+            continuous_square()
+            discrete_square()
+        else:
+            print("Tipo de señal no reconocido. Usa: seno, exponencial, triangular o cuadrada.")
+
+    elif options[1] == "Tarea2":
+        if len(options) > 2:
             understanding_freq(options[2])
         else:
-            print("Please give a frequency. Example: python main.py act_2 2")
+            print("Por favor proporciona una frecuencia. Ejemplo: python main.py Tarea2 2")
+
+    else:
+        print("Opción no reconocida. Usa: Tarea1 o Tarea2")
 
 
 if __name__ == '__main__':
@@ -19,6 +46,10 @@ if __name__ == '__main__':
     if len(args) > 1:
         main(args)
     else:
-        print("Please give an argument")
-        print("Example ( run activity 1 ) : python main.py act_1")
-        print("Example ( run activity 2 ) : python main.py act_2 1")
+        print("Por favor proporciona un argumento")
+        print("Ejemplos:")
+        print("  python main.py Tarea1 seno")
+        print("  python main.py Tarea1 exponencial")
+        print("  python main.py Tarea1 triangular")
+        print("  python main.py Tarea1 cuadrada")
+        print("  python main.py Tarea2 2")
