3701f0b28fbd3d72456062ded03920da5e7bc68e
JesRey09 <jesusjarf2004@gmail.com>
2025-05-29 23:51:11 -0600

    add_file

--- src/Tarea2.py
+++ src/Tarea2.py
@@ -3,13 +3,13 @@ from src.utils.grapher import continuous_plotter
 
 
 def understanding_freq(des_freq):
-    initial_time = 0
-    end_time = 5
-    frequency = float(des_freq)
-    amplitude = 1
-    number_of_points = 1000
-    time = np.linspace(initial_time, end_time, number_of_points)
-    xt = amplitude * np.sin(2 * np.pi * frequency * time)
+    initial_time = 0  #seleccion del tiemo inicla de 0 segundos 
+    end_time = 5     #Seleccion de tiempo final de 5 segundos 
+    frequency = float(des_freq)   #Toma la frecuencia dada y la concierte a decimal 
+    amplitude = 1             #Amplitud de la señal senoidal
+    number_of_points = 1000  # Número de puntos del vector de tiempo
+    time = np.linspace(initial_time, end_time, number_of_points)  #Vector de tiempo continuo de 0 a 5 con los 1000 puntos 
+    xt = amplitude * np.sin(2 * np.pi * frequency * time) #Señal sinusoidal de la forma sin(2πft)
     continuous_plotter(time, xt,
     'Continuous Sine wave Signal', 'Sin wave Signal',
     'Time [s]', 'Amplitude')
--- src/tarea1.py
+++ src/tarea1.py
@@ -4,15 +4,15 @@ from src.utils.grapher import continuous_plotter, discrete_plotter
 
 # 1. Señal senoidal continua
 def continuous_sine():
-    frequency = 2
-    amplitude = 1
-    number_of_points = 10000
+    frequency = 2    #Seleccionamos la frecuencia en hz
+    amplitude = 1    #Amplitud dada para la Señal senoidal continua
+    number_of_points = 10000  #El numero de puntos dados para la señal discreta 
     time_initial = -1
-    time_final = 5
-    time = np.linspace(time_initial, time_final, number_of_points)
-    x_t = amplitude * np.sin(2 * np.pi * frequency * time)
+    time_final = 5  
+    time = np.linspace(time_initial, time_final, number_of_points) #Vector de tiempo continuo 
+    x_t = amplitude * np.sin(2 * np.pi * frequency * time) # Señal senoidal sin(2πft)
 
-    continuous_plotter(
+    continuous_plotter(  #Se usa para graficar la senal de tiempo continuo que entra por el argumento 
         time, x_t,
         'Continuous Sine wave Signal', 'x₁(t) = sin(2π·f·t)',
         'Time [s]',  'Amplitude')
@@ -20,12 +20,12 @@ def continuous_sine():
 
 # 2. Señal senoidal discreta
 def discrete_sine():
-    frequency = 2
-    amplitude = 1
-    fs = 20
-    ts = 1 / fs
-    samples = 100
-    n = np.arange(samples)
+    frequency = 2 #Seleccionamos la frecuencia en hz 
+    amplitude = 1  #Amplitud dada para la Señal senoidal discreta
+    fs = 20   #Frecuencia de muestreo 20
+    ts = 1 / fs #Peridodo de muestreo 
+    samples = 100 #Indices de muestras de 0 a 99
+    n = np.arange(samples) #señal muestreada
     x_n = amplitude * np.sin(2 * np.pi * frequency * n * ts)
 
     discrete_plotter(
@@ -38,8 +38,8 @@ def discrete_sine():
 def continuous_exponential():
     number_of_points = 1000
     time = np.linspace(-1, 5, number_of_points)
-    u = np.heaviside(time, 1)  # u(t)
-    x_t = np.exp(-2 * time) * u
+    u = np.heaviside(time, 1)  # Señal escalón unitario u(t)
+    x_t = np.exp(-2 * time) * u  #Señal x(t) = e^(–2t) · u(t)
 
     continuous_plotter(
         time, x_t,
@@ -50,11 +50,11 @@ def continuous_exponential():
 # 4. Señal exponencial discreta
 def discrete_exponential():
     samples = 100
-    ts = 0.05
-    n = np.arange(-20, samples)
-    t = n * ts
-    u = np.heaviside(t, 1)
-    x_n = np.exp(-2 * t) * u
+    ts = 0.05            #Periodo de muestreo 
+    n = np.arange(-20, samples)   #Indices de muestra que van desde n = -20
+    t = n * ts                    #Equivalencia para cada muestra 
+    u = np.heaviside(t, 1)        #Escalon unitario 
+    x_n = np.exp(-2 * t) * u      #Señal discreta exponencial
 
     discrete_plotter(
         n, x_n,
@@ -66,9 +66,9 @@ def discrete_exponential():
 def continuous_triangle():
     from scipy import signal
     frequency = 2
-    number_of_points = 1000
-    time = np.linspace(0, 2, number_of_points)
-    x_t = signal.sawtooth(2 * np.pi * frequency * time, 0.5)  # 0.5 → forma triangular
+    number_of_points = 1000 # Número de puntos para crear la señal suave la cual no tiene cambios bruscos 
+    time = np.linspace(0, 2, number_of_points) # Vector de tiempo continuo de 0 a 2 segundos
+    x_t = signal.sawtooth(2 * np.pi * frequency * time, 0.5)  # 0.5 → para al forma triangular simetrica 
 
     continuous_plotter(
         time, x_t,
@@ -81,10 +81,10 @@ def discrete_triangle():
     from scipy import signal
     frequency = 2
     fs = 100
-    ts = 1 / fs
-    samples = 200
-    n = np.arange(samples)
-    t = n * ts
+    ts = 1 / fs # Periodo de muestreo
+    samples = 200   # Número total de muestras
+    n = np.arange(samples)  # Vector de índices n = 0 a 199
+    t = n * ts     # Vector de tiempos discreto para convertir n a tiempo real
     x_n = signal.sawtooth(2 * np.pi * frequency * t, 0.5)
 
     discrete_plotter(
@@ -97,9 +97,9 @@ def discrete_triangle():
 def continuous_square():
     from scipy import signal
     frequency = 2
-    number_of_points = 1000
-    time = np.linspace(0, 2, number_of_points)
-    x_t = signal.square(2 * np.pi * frequency * time)
+    number_of_points = 1000  # Cantidad de puntos en el vector tiempo
+    time = np.linspace(0, 2, number_of_points) # Vector de tiempo de 0 a 2 segundos
+    x_t = signal.square(2 * np.pi * frequency * time) # Pafa generar la señal cuadrada
 
     continuous_plotter(
         time, x_t,
@@ -115,7 +115,7 @@ def discrete_square():
     ts = 1 / fs
     samples = 200
     n = np.arange(samples)
-    t = n * ts
+    t = n * ts   # Tiempo asociado a cada muestra
     x_n = signal.square(2 * np.pi * frequency * t)
 
     discrete_plotter(
